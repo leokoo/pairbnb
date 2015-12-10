8c9c7bd750c40e42b11a582fab960b050619e55f
Leo Koo <leokoo@gmail.com>
2015-12-10 18:20:41 +0800

    Final Changes

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,5 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, ENV['FACEBOOK_KEY'], ENV['FACEBOOK_SECRET'],
+  provider :facebook, ENV['facebook_id'], ENV['facebook_secret'],
            :scope => 'public_profile,email,user_birthday', :display => 'popup'
 # OmniAuth.config.logger = Rails.logger
 end
