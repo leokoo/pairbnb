fa518c6e0e96280b8be93bdb5f8f773390310aeb
Leo Koo <leokoo@gmail.com>
2015-12-17 12:20:35 +0800

    Categories validation done

--- app/models/plugin.rb
+++ app/models/plugin.rb
@@ -1,8 +1,8 @@
 class Plugin < ActiveRecord::Base
 
-	belongs_to :user
-	validates :plugin_name, presence: true
 
+	validates :plugin_name, presence: true
+	belongs_to :user
 	has_many :plugin_categories
 	has_many :categories, through: :plugin_categories
 	accepts_nested_attributes_for :categories, allow_destroy: true
