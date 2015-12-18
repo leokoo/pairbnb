ecb18905d19630d315822bf3db769e5479c9a5ce
Leo Koo <leokoo@gmail.com>
2015-12-18 15:47:52 +0800

    Added CarrierWave Gem

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,5 +1,5 @@
 class Category < ActiveRecord::Base
-	# validates :name, uniqueness: true
+	# validates :name, presence: true
 	has_many :plugin_categories
 	has_many :plugins, through: :plugin_categories
 	accepts_nested_attributes_for :plugins, allow_destroy: true
