3fd1a54954fc001283c02df90c0b43249d548c25
Leo Koo <leokoo@gmail.com>
2015-12-19 17:06:56 +0800

    user RESTful pt2

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,42 @@
 class UsersController < Clearance::UsersController
   before_action :require_login, only: :index
+  before_action :set_users, only: [:show, :edit, :update, :destroy]
 
   def index
+  	@users = User.all
   end
 
   def dashboard
   end
+
+  def show
+  end
+
+ 	def edit
+ 	end
+
+ 	def update
+ 		if @user.update(user_params)
+ 			redirect_to @user
+ 		else
+ 			render :edit
+ 		end
+ 	end
+
+ 	def destroy
+ 		@user.destroy
+
+ 		redirect_to users_path
+ 	end
+
+	private
+	def user_params
+		params.require(:user).permit(:first_name, :email)
+	end
+
+	def set_user
+		@user = User.find(params[:id])
+	end
 end
+end
+
