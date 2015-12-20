98ca1c4f583e930911313f4ca96e4afe00973ab5
Leo Koo <leokoo@gmail.com>
2015-12-21 02:50:25 +0800

    images done

--- db/migrate/20151220175611_create_post_attachments.rb
+++
@@ -1,9 +0,0 @@
-class CreatePostAttachments < ActiveRecord::Migration
-  def change
-    create_table :post_attachments do |t|
-    	t.string :image
-
-      t.timestamps null: false
-    end
-  end
-end
